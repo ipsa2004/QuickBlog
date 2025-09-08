2a7ba898e26478bdb03c284bbda34324dcd21b9c
ipsa2004 <ipsasahoo966@gmail.com>
2025-09-09 00:14:25 +0530

    Your commit message here

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import {Routes, Route } from 'react-router-dom'
+import Blog from './pages/Blog'
+import Home from './pages/Home'
+
+const App = () => {
+  return (
+    <div>
+        <Routes>
+            <Route path='/' element = {<Home/>}/>
+            <Route path='/blog/:id' element = {<Blog/>}/>
+        </Routes>
+
+
+    </div>
+  )
+}
+
+export default App
