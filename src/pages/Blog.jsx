f20266a37b19336ed66b2dd7a6f275feb6c25d00
ipsa2004 <ipsasahoo966@gmail.com>
2025-09-16 11:56:33 +0530

    login page

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -3,11 +3,15 @@ import { useParams} from 'react-router-dom'
 import Navbar from '../components/Navbar'
 import { assets , blog_data, comments_data} from '../assets/assets'
 import Moment from 'moment'
+import Footer from '../components/Footer'
+import Loader from '../components/Loader'
 
 const Blog = () => {
   const {id} = useParams()
   const [data, setData] = useState(null)
   const [comments, setComments] = useState([])
+  const [name, setName] = useState('')
+  const [content, setContent] = useState('')
 
   const fetchBlogData = async() =>{
     const data = blog_data.find(item => item._id === id)
@@ -18,6 +22,10 @@ const Blog = () => {
     setComments(comments_data)
   }
 
+  const addComment = async(e)=> {
+    e.preventDefault();
+  }
+
   useEffect(() =>{
     fetchBlogData()
     fetchComments()
@@ -25,7 +33,7 @@ const Blog = () => {
 
    return data ? (
     <div className='relative'>
-      <img src={assets.gradientBackground} alt="" className='absolute -top-50 -z--1 opacity-50'/>
+      <img src={assets.gradientBackground} alt="" className='absolute -top-50 -z-10 opacity-50'/>
       <Navbar/>
       <div className='text-center mt-20 text-gray-600'>
         <p className='text-primary py-4 font-medium'>Published on {Moment(data.createdAt).format('MMMM Do YYYY')}</p>
@@ -41,7 +49,7 @@ const Blog = () => {
 
       {/*comment section--- */}
       <div className='mt-14 mb-10 max-w-3xl mx-auto'>
-        <p>Comments ({comments.length})</p>
+        <p className='font-semibold mb-4'>Comments ({comments.length})</p>
         <div className='flex flex-col gap-4'>
           {comments.map((item, index) =>(
             <div key ={index} className='relative bg-primary/2 border border-primary/5 max-w-xl p-4 rounded text-gray-600'>
@@ -53,12 +61,28 @@ const Blog = () => {
               <div className='absolute right-4 bottom-3 flex items-center gap-2 text-xs'>{Moment(item.createdAt).fromNow()}</div>
             </div>
           ))}
-
         </div>
-
       </div>
+
+      <div className='max-w-3xl mx-auto'>
+        <p className='font-semibold mb-4'>Add your comment</p>
+        <form onSubmit={addComment} className='flex flex-col items-start gap-4 max-w-lg'>
+          <input onChange={(e) => setName(e.target.value)} value={name} type="text" placeholder='Name' required className='w-full p-2 border border-gray-300 rounded outline-none'/>
+          <textarea onChange={(e) => setContent(e.target.value)} value={content} placeholder='Comment' required className='w-full p-2 border border-gray-300 rounded outline-none h-48'></textarea>
+          <button type="submit" className='bg-primary text-white rounded p-2 px-8 hover:scale-102 transition-all cursor-pointer'>Submit</button>
+        </form>
+      </div>
+      <div className='my-24 max-w-3xl mx-auto'>
+        <p className='font-semibold my-4'>Share this article on social media</p>
+        <div className='flex'>
+          <img src={assets.facebook_icon} width={50} alt=''/>
+          <img src={assets.twitter_icon} width={50} alt=''/>
+          <img src={assets.googleplus_icon} width={50} alt=''/>
+        </div>
+      </div>
+      <Footer/>
     </div>
-  ) : <div>Loading...</div>
+  ) : <Loader/>
 }
 
 export default Blog
